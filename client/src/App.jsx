ba7ab4262734220dab5272aa5d4d5e1b5613b11e
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-06-24 07:29:04 +0530

    basic SetUp

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+
+
+
+  return (
+   <Button> LEt's build LMS :</Button>
+  )
+}
+
+export default App
